3e9b991641676c23d5247f69f1f02bea92bd902f
Aakash kumar <aakashk7092@gmail.com>
2026-06-05 23:59:56 +0530

    Add color sorting algorithm in SortColor.cpp
    Implemented the Dutch National Flag algorithm to sort colors in a given array.

---
+++ Arrays/SortColor.cpp
@@ -0,0 +1,25 @@
+// Optimal Approach 
+// time Complexity = O(n);
+// space complexity = (1)
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int  i = 0 ;
+        int j =  0 ;
+        int k = nums.size()-1;
+
+        while(j <= k){
+            if(nums[j] == 1){
+                j++;
+            }else if(nums[j] == 2){
+                swap(nums[j],nums[k]);
+                k--;
+            }else{
+                swap(nums[i],nums[j]);
+                i++;
+                j++;
+            }
+        }
+    }
+};
