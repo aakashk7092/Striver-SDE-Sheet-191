e42fd75ccbbecd2b31e72d6a6a3c3e5a96333b89
Aakash kumar <aakashk7092@gmail.com>
2026-06-05 13:49:14 +0530

    Add maximum subarray solution using Kadane's algorithm
    Implement Kadane's algorithm to find the maximum subarray sum.

---
+++ Arrays/maximumSubarray.cpp
@@ -0,0 +1,19 @@
+// kadane's algorithm 
+// time complexity - O(n)
+// space complexity - O(1)
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int currSum=0 , maxSum= INT_MIN;
+        for(int val:nums){
+            currSum+=val;
+            maxSum= max(currSum,maxSum);
+            if(currSum<0){
+                currSum=0;
+            }
+        }
+        return maxSum;
+        
+    }
+};
